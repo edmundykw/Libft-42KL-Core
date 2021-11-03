92cb442570452024c063efae887c770663ef3d8d
Edmund yap Keen wye <ekeen-wy@u82z05s01.42kl.edu.my>
2021-11-03 17:18:47 +0800

    Latest

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: ekeen-wy <ekeen-wy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 14:29:42 by ekeen-wy          #+#    #+#             */
-/*   Updated: 2021/11/03 16:55:58 by ekeen-wy         ###   ########.fr       */
+/*   Updated: 2021/11/03 17:18:39 by ekeen-wy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,11 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	char	*ptr;
 	int		i;
+	int		j;
 
 	ptr = dst;
 	i = ft_strlen((char *) src);
+	j = ft_strlen((char *) dst);
 	if (dst == NULL && src == NULL)
 		return (dst);
 	if (dst < src)
