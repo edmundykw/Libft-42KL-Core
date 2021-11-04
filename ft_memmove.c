15645b0941df95c0efe397f9e709833a9370dc17
edmundykw <edmundykw@gmail.com>
2021-11-04 10:55:01 +0800

    Latest

--- ft_memmove.c
+++ ft_memmove.c
@@ -3,28 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ekeen-wy <ekeen-wy@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ekeen-wy <ekeen-wy@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 14:29:42 by ekeen-wy          #+#    #+#             */
-/*   Updated: 2021/11/03 17:18:39 by ekeen-wy         ###   ########.fr       */
+/*   Updated: 2021/11/04 10:46:52 by ekeen-wy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memmove(void *dst, const void *src, size_t len)
+void	*ft_memmove(void *dest, const void *src, size_t len)
 {
-	char	*ptr;
-	int		i;
-	int		j;
+	unsigned char	*ptr;
 
-	ptr = dst;
-	i = ft_strlen((char *) src);
-	j = ft_strlen((char *) dst);
-	if (dst == NULL && src == NULL)
-		return (dst);
-	if (dst < src)
-	{}
-	ft_memcpy(dst, src, len);
-	return (dst);
+	ptr = dest;
+	if (dest == NULL && src == NULL)
+		return (dest);
+	if (dest < src)
+	{
+		while (len-- != 0)
+		{
+			*ptr = *(unsigned char *) src;
+			ptr++;
+			src++;
+		}
+	}
+	else
+	{
+		while (len-- != 0)
+		{
+			*(ptr + len) = *(unsigned char *)(src + len);
+		}
+	}
+	return (dest);
 }
--- libft.h
+++ libft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ekeen-wy <ekeen-wy@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ekeen-wy <ekeen-wy@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/23 12:32:20 by ekeen-wy          #+#    #+#             */
-/*   Updated: 2021/11/02 15:23:01 by ekeen-wy         ###   ########.fr       */
+/*   Updated: 2021/11/04 10:50:14 by ekeen-wy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,5 +29,5 @@ int				ft_atoi(char *str);
 void			*ft_memset(void *s, int c, size_t n);
 void			*ft_memcpy(void *dest, const void *src, size_t n);
 void			*ft_memmove(void *dst, const void *src, size_t len);
-int	            ft_memcmp(const void *s1, const void *s2, size_t n);
+int				ft_memcmp(const void *s1, const void *s2, size_t n);
 #endif
