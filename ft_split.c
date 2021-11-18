a0468303f1cb78b3d39e570fa664a80a71cebf9b
edmundykw <edmundykw@gmail.com>
2021-11-18 09:37:11 +0800

    ft_split.c

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: ekeen-wy <ekeen-wy@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/16 10:13:05 by ekeen-wy          #+#    #+#             */
-/*   Updated: 2021/11/17 21:10:47 by ekeen-wy         ###   ########.fr       */
+/*   Updated: 2021/11/18 09:36:34 by ekeen-wy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,14 +26,30 @@ static unsigned int	word_count(char *s, char c)
 		while (*ptr != c && *ptr != '\0')
 		{
 			ptr++;
-			if (*ptr == c)
-				k++;
-			if (k > 0 && ptr == (s + j))
+			if (*ptr == c || (k > 0 && ptr == (s + j)))
 				k++;
 		}
 		ptr++;
 	}
 	return (k);
+}
+
+static char	*ft_strndup(const char *s, size_t len)
+{
+	char	*ptr;
+	int		i;
+
+	i = 0;
+	ptr = (char *)malloc(sizeof(*ptr) * (ft_strlen((char *) s) + 1));
+	if (sizeof(ptr) < sizeof(s) || ptr == NULL)
+		return (NULL);
+	while (s[i] != '\0' && len-- > 0)
+	{
+		ptr[i] = s[i];
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
 }
 
 char	**ft_split(char const *s, char c)
@@ -45,21 +61,19 @@ char	**ft_split(char const *s, char c)
 
 	if (s == NULL)
 		return (NULL);
-	ptr = ft_calloc((word_count((char *) s, c) + 1), sizeof(ptr));
-	i = 0;
+	ptr = ft_calloc(word_count((char *) s, c) + 1, sizeof(*ptr));
+	j = ft_strlen((char *) s);
 	k = 0;
-	while (*(s + i) != '\0')
+	while (*s != '\0')
 	{
-		j = 0;
-		if (*(s + i) != c)
+		i = 0;
+		while (*(s + i) != c && *(s + i) != '\0')
 		{
-			while (*(s + i + j) != c)
-				j++;
-			*(ptr + k) = ft_substr((char *) s, i, j);
-			i += j;
-			k++;
+			i++;
+			if (*(s + i) == c || (k > 0 && s == (s + j)))
+				ptr[k++] = ft_strndup((char *) s, i);
 		}
-		i++;
+		s++;
 	}
 	*(ptr + k) = NULL;
 	return (ptr);
