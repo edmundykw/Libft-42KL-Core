bbe61f448155fe411d5eee99a5609b2cd0c4e38b
edmundykw <edmundykw@gmail.com>
2021-11-19 11:06:56 +0800

    Latest

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: ekeen-wy <ekeen-wy@student.42kl.edu.my>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/10/15 10:06:07 by ekeen-wy          #+#    #+#              #
-#    Updated: 2021/11/16 10:18:37 by ekeen-wy         ###   ########.fr        #
+#    Updated: 2021/11/18 22:51:19 by ekeen-wy         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -22,7 +22,7 @@ SRC = ft_isalpha.c ft_isdigit.c ft_isalnum.c ft_isascii.c ft_isprint.c ft_strlen
 	  ft_toupper.c ft_tolower.c ft_strlcpy.c ft_strlcat.c ft_strncmp.c ft_atoi.c \
 	  ft_memset.c ft_memcpy.c ft_memmove.c ft_memcmp.c ft_memchr.c ft_bzero.c \
 	  ft_strchr.c ft_strrchr.c ft_strnstr.c ft_strdup.c ft_calloc.c ft_substr.c \
-	  ft_strjoin.c ft_split.c
+	  ft_strjoin.c ft_strtrim.c	ft_split.c 
 	  
 OBJ = $(SRC:.c=.o)
 
---
+++ ft_strtrim.c
@@ -0,0 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ekeen-wy <ekeen-wy@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/18 22:50:50 by ekeen-wy          #+#    #+#             */
+/*   Updated: 2021/11/19 11:04:59 by ekeen-wy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static unsigned int	front_trim(const char *s1, const char *s2)
+{
+	unsigned int	i;
+	unsigned int	j;
+	unsigned int	index;
+
+	i = 0;
+	index = 0;
+	while (s1[i] != '\0')
+	{
+		j = 0;
+		while (s2[j] != '\0')
+		{
+			if (s1[i] == s2[j])
+				index++;
+			j++;
+		}
+		if (index > i)
+			i++;
+		else
+			i = ft_strlen((char *) s1);
+	}
+	return (index);
+}
+
+static unsigned int	back_trim(const char *s1, const char *s2)
+{
+	unsigned int	i;
+	unsigned int	j;
+	unsigned int	index;
+
+	i = ft_strlen((char *) s1) - 1;
+	while (i > 0)
+	{
+		j = 0;
+		index = i;
+		while (s2[j] != '\0')
+		{
+			if (s1[i] == s2[j])
+				index--;
+			j++;
+		}
+		if (i > index)
+			i--;
+		else
+			i = 0;
+	}
+	return (index);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	char			*ptr;
+	unsigned int	start;
+	unsigned int	end;
+	unsigned int	len;
+
+	if (s1 == NULL || set == NULL)
+		return (NULL);
+	if (ft_strlen((char *) s1) == 0 || ft_strlen((char *) set) == 0)
+		return ("");
+	start = front_trim(s1, set);
+	end = back_trim(s1, set);
+	len = end - start + 1;
+	if (start > len)
+		len = 1;
+	ptr = ft_substr(s1, start, len);
+	if (ptr == NULL)
+		return (NULL);
+	return (ptr);
+}
